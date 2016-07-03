909bfaf31686d304a3cec170839977e1f58ea3ef
Anton Butenko <ant.butenko@gmail.com>
2016-07-03 23:52:23 +0300

    added logout and parametrized pytest test (not finished)

--- framework/login.py
+++ framework/login.py
@@ -1,52 +1,41 @@
 # -*- coding: utf-8 -*-
 import time
+
+from tools import wait_for_page_load, page_contain_assert
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.support.wait import WebDriverWait
 
 
-class wait_for_page_load(object):
-
-    def __init__(self, browser):
-        self.browser = browser
-
-    def __enter__(self):
-        self.old_page = self.browser.find_element_by_tag_name('html')
-
-    def wait_for(self, condition_function):
-        start_time = time.time()
-        while time.time() < start_time + 3:
-            if condition_function():
-                return True
-            else:
-                time.sleep(0.1)
-        raise Exception(
-            'Timeout waiting for {}'.format(condition_function.__name__)
-        )
-
-    def page_has_loaded(self):
-        new_page = self.browser.find_element_by_tag_name('html')
-        return new_page.id != self.old_page.id
-
-    def __exit__(self, *_):
-        self.wait_for(self.page_has_loaded)
+def enter_login_credentials(driver, username, password):
+    name = driver.find_element_by_name("username")
+    name.send_keys(username)
+    psswd = driver.find_element_by_name("password")
+    psswd.send_keys(password)
+    return psswd
 
 
 def login(driver, username="user@phptravels.com", password="demouser", login_page="http://www.phptravels.net/login"):
     if login_page:
         driver.get(login_page)
-        assert "Login" in driver.title
-    name = driver.find_element_by_name("username")
-    name.send_keys(username)
-    psswd = driver.find_element_by_name("password")
-    psswd.send_keys(password)
+    page_contain_assert(driver, title=["Login"], page_source=["Email", "Password", "Remember Me",
+                                                    "Login", "Sign Up", "Forget Password"])
+    psswd = enter_login_credentials(driver, username, password)
     with wait_for_page_load(driver):
         psswd.send_keys(Keys.RETURN)
     # wait = WebDriverWait(driver, 2)
     # wait.until(EC.title_is("My Account"))
-    assert "My Account" in driver.title
-    assert "Hi, " in driver.page_source
+    page_contain_assert(driver, title=["My Account"], page_source=["Hi, ", "Booking",
+                                                                   "My Profile", "Wishlist", "Newsletter"])
+
+
+def logout(driver, logout_page="http://phptravels.net/account/logout/"):
+    if logout_page not in driver.page_source:
+        login(driver)
+    page_contain_assert(driver, page_source=[logout_page])
+    driver.get(logout_page)
+    page_contain_assert(driver, title=["Login"])
 
 
 if __name__ == "__main__":
--- test_login.py
+++ test_login.py
@@ -1,12 +1,42 @@
 # -*- coding: utf-8 -*-
-from framework.login import login
+from selenium.webdriver.common.keys import Keys
+
+from framework import login, tools
 from selenium import webdriver
 import pytest
 
-def test_default_user():
+
+def enter_login_page(driver):
+    driver.get("http://www.phptravels.net/login")
+
+
+def test_login_default_user():
     driver = webdriver.Chrome()
     try:
-        login(driver)
+        login.login(driver)
     finally:
         driver.close()
 
+
+@pytest.mark.parametrize("user, psswd",[('',''), ('user@phptravels.com', '')])
+def test_login_any_user(user, psswd):
+    driver = webdriver.Chrome()
+    try:
+        enter_login_page(driver)
+        login.page_contain_assert(driver, title=["Login"], page_source=["Email", "Password", "Remember Me",
+                                                        "Login", "Sign Up", "Forget Password"])
+        psswd = login.enter_login_credentials(driver, user, psswd)
+        # with tools.wait_for_page_load(driver):
+        psswd.send_keys(Keys.RETURN)
+        #TODO
+
+    finally:
+        driver.close()
+
+
+def test_logout_default_user():
+    driver = webdriver.Chrome()
+    try:
+        login.logout(driver)
+    finally:
+        driver.close()
